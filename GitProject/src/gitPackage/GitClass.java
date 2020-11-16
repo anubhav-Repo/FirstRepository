c39e502f5d355b69fba19f2ffdc72b2a93fbbe5e
anubhav-Repo <tyagianubhav504@gmail.com>
2020-11-16 22:37:42 +0530

    Generating conflict

--- GitProject/src/gitPackage/GitClass.java
+++ GitProject/src/gitPackage/GitClass.java
@@ -4,7 +4,7 @@ public class GitClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Initializing........");
+		System.out.println("Initializing after merging........");
 		System.out.println("Under Development Branch...");
 		System.out.println("Under Development1 Branch...");
 
